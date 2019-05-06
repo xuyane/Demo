7ac6094baeec6a085ed49504edff71c83b987dbe
xiaobai <419456942@qq.com>
2019-05-06 19:28:59 +0800

    轮播图

---
+++ Travel_where/Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,27 @@
+<template>
+  <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide>
+      <img src="https://source.qunarzz.com/site/images/wns/20190415_qunar_dujia_homepage_top_banner_1.jpeg">
+    </swiper-slide>
+    <swiper-slide>
+      <img src="https://source.qunarzz.com/site/images/wns/2019505_dujia_homepage_top_banner_5.jpg">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data() {
+    return {
+      swiperOption: {}
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
